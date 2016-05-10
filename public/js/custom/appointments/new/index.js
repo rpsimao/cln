37fe9c8c4cf378f162c59ed671ad93d2018bab5a
Ricardo Simão <rpsimao@gmail.com>
2016-05-10 10:02:43 +0100

    New Appointment Finalized

--- public/js/custom/appointments/new/index.js
+++ public/js/custom/appointments/new/index.js
@@ -1,35 +1,44 @@
 $(function() {
 
 
-    var select = $("#select2-disabled-inputs-single");
+    var select = $("#type_treatment");
+    var futureSelect = $("#future-body-zone");
+    var typeSkin = $("#type_of_skin");
 
 
-    select.on('change', function() {
+    select.on('change', function() {populateTreatmentSelect(select.val(), "typeOfTreatmentSelect");});
 
+    futureSelect.on('change', function() {populateTreatmentSelect(futureSelect.val(), "typeOfTreatmentFutureSelect");});
 
-
-        populateTreatmentSelect(select.val());
-
-
-
-    });
+    typeSkin.on('change', function() {alertTypeOfSkin("type_of_skin")});
+    
+    
 
     
 });
 
-function populateTreatmentSelect(values)
-
+function populateTreatmentSelect(values, id)
 {
+    var select = $("#"+id);
 
-    var select = $("#typeOfTreatmentSelect");
-
-    sendData = $.post( "/appointments/new/lookfortreatments", { treat: values} );
+    sendData = $.post("/appointments/new/lookfortreatments", {treat: values});
 
     sendData.done(function(data){
-
         select.html(data);
-
-
     });
 
 }
+
+function alertTypeOfSkin(id) {
+
+    var typeSkin = $("#"+id);
+
+
+    if (typeSkin.val() == "Black" || typeSkin.val() == "Mate"){
+
+        openModal("modal-2");
+
+    }
+    
+}
+
